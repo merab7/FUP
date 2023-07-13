45f77ef53ee435e199116862ad3892428cc9679d
unknown <merabtodua7@gmail.com>
2023-07-13 19:28:46 +0400

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,28 @@
 import "@styles/globals.css";
+
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
-export const metadata ={
-    title: "FUP",
-    description: "Discover and Share AI Prompts"
-}
-const RootLayout=({children})=> {
-  return (
-    
-      <html lang="en"  >
-        <body>
-            <Provider>
-            <div className="main" >
-                <div className="gradient" />
-            </div>
-            <main className="app" >
-                <Nav/>
-                {children}
-            </main>
-            </Provider>
-        </body>
-      </html>
-    
-  )
-}
 
-export default RootLayout
+export const metadata = {
+  title: "Promptfy",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
